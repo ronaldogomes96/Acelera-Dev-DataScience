24ea343ab4f9a6d8775d21eca8e4f6898fc4d802
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-06-17 16:24:37 -0300

    Manipulando dados II - Listas e dataframe

--- Modulo 2/aceleraDev_Semana2.py
+++ Modulo 2/aceleraDev_Semana2.py
@@ -26,3 +26,71 @@ print(dados['canal_de_venda'][2])
 
 #Acessando posicoes especificas de um dicionario com intervalo
 print(dados['canal_de_venda'][:2])
+
+
+#Listas
+
+#Verificando se é uma lista 
+print(type([1,2,3]))
+
+#Criando uma lista
+lista = [200, 200 , 300 ,800, 200]
+
+#Printando a lista
+print(lista)
+
+#Vendo valores especificos
+print(lista[1])
+
+#Fatia de lista
+print(lista[:2])
+
+#Adicionando a lista ao dicionario
+dados['lista'] = lista
+
+print(dados)
+
+#Data Frames
+
+#Criando um data frame a partir de um dicionario
+dataframe = pd.DataFrame(dados)
+
+#printando o data frame
+print(dataframe)
+
+#Printando os 2 primeiros casos do data frame
+print(dataframe.head(2))
+
+#Verificando o formato do data frame
+print(dataframe.shape)
+
+#Verificando o indice do dataframe
+print(dataframe.index)
+
+#Verificando os tipos dos dados do dataframe
+print(dataframe.dtypes)
+print(dataframe.dtypes.value_counts())
+
+#Verificando se existem valores faltantes, mostrando a tabela
+print(dataframe.isna())
+
+#Mostra o numero de acordo com a varival
+print(dataframe.isna().sum())
+
+#printando os nomes das colunas
+print(dataframe.columns)
+
+#Acessando uma coluna especifica
+print(dataframe['canal_venda'])
+
+# Criando uma nova coluna
+dataframe['nova_coluna'] = [1, 2, 3, 4, 5]
+
+#Mostrando novamente as colunas
+print(dataframe.columns)
+
+# Removendo colunas
+dataframe.drop(columns = ['acessos','site', 'canal_venda'])
+#Ele so elemina no data frame originial se usar o parametro inplace = true
+
+
