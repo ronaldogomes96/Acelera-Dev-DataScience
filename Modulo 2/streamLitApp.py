51524a856b260f9876da3d2db2feb34a82e0fe69
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-06-18 22:22:03 -0300

    Streamlit App I e II - Conceitos iniciais de streamlit

---
+++ Modulo 2/streamLitApp.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+
+def main():
+    st.title("Hello World") #Titulo
+    st.header("This is a header")
+    st.subheader("Isso é uma subheader")
+    st.text("Isso é um texto")
+    #st.image("Nome da imagem")
+    #st.audio("nome do audio")
+    #st.video("nome do video")
+    
+if __name__ == "__main__":
+    main()
+
