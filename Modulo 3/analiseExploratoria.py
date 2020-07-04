e4999af51cc279c3a2b0528e3eef3b8aa8d72a6a
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-04 14:32:36 -0300

    Estatistica univariada

--- Modulo 3/analiseExploratoria.py
+++ Modulo 3/analiseExploratoria.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import matplotlib
 
 df = pd.read_csv('houses_to_rent_v2.csv')
 
@@ -14,3 +15,18 @@ print(df.isna().sum())
 
 #Vendo as informacoes do data frame
 print(df.info())
+
+#Estatistica Univariada
+
+#Mudando o nome do target
+df.rename(columns = {'rent amount (R$)' : 'valor_aluguel'}, inplace = True)
+print(df['valor_aluguel'])
+
+#Algumas medidas estatisticas
+print(df['valor_aluguel'].mean())
+print(df['valor_aluguel'].median())
+print(df['valor_aluguel'].std())
+print(df['valor_aluguel'].describe())
+
+#Plot dos dados com o pandas
+print(df['valor_aluguel'].plot(kind = 'hist', bins = 10))
