499fd68212b88165076c9a594e99df3f83c01dde
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-05 21:11:44 -0300

    Desafio do modulo 3

---
+++ Modulo 3/desafioModulo3.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import json
+
+df = pd.read_csv('baseDesafio.csv')
+
+#Selecionando as colunas para o uso
+dadosImportantes = { 'pontuacao_credito' : df['pontuacao_credito'] ,
+                     'estado_residencia' : df['estado_residencia']}
+
+#Criando um novo datframe com essa colunas
+data = pd.DataFrame(dadosImportantes)
+
+#Lendo o arquivo json
+with open ('submissionTeste.json','r') as arquivo:
+    texto = arquivo.read()
+    submissionJSON = json.loads(texto)
+submissionJSON['SC']['moda'] = 10
+
+#Recebendo os valores necessarios
+moda = df.groupby('estado_residencia')['pontuacao_credito'].apply(lambda x: x.mode())
+media = df.groupby('estado_residencia')['pontuacao_credito'].mean().reset_index()
+mediana = df.groupby('estado_residencia')['pontuacao_credito'].median().reset_index()
+desvioPadrao = df.groupby('estado_residencia')['pontuacao_credito'].std().reset_index()
+
+#Analisando os resultados
+print(moda)
+print(mediana)
+print(media)
+print(desvioPadrao)
