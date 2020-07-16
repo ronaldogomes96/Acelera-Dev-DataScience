c5155a6372b0ceb4eb5d1ad41f96ffb4aea12d15
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-16 13:17:30 -0300

    Arquivos do desafio do modulo 6

---
+++ Modulo 6/analiseDadosDesafio.py
@@ -0,0 +1,13 @@
+from math import sqrt
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as sct
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.stats as st
+from sklearn.decomposition import PCA
+from loguru import logger
+from IPython.core.pylabtools import figsize
+
+
